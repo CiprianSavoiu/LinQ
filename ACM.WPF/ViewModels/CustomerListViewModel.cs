a97de0c65793a788e0d94a2525ce3774aa6aeaa7
Ciprian Savoiu <ciprian.savoiu@nmgroup.com>
2017-11-22 01:01:20 +0000

    part 2 - wpf project

---
+++ ACM.WPF/ViewModels/CustomerListViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ACM.BL;
+
+namespace ACM.WPF.ViewModels
+{
+    public class CustomerListViewModel: ViewModelBase
+    {
+        public ObservableCollection<Customer> _Customers;
+
+        public ObservableCollection<Customer> Customers
+        {
+            get { return _Customers; }
+            set
+            {
+                if (_Customers != value)
+                {
+                    _Customers = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        CustomerRepository customerRepository = new CustomerRepository();
+
+        public CustomerListViewModel()
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            _Customers = new ObservableCollection<Customer>();
+            var customerList = customerRepository.Retrieve();
+            foreach (var customerInstance in customerList)
+            {
+                _Customers.Add(customerInstance);
+            }
+        }
+    }
+}
